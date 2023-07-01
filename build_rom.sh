7c13d6cff5ceaa5a2390ab3748a1c1a87bc0e259
acex69 <abhinav.115260@gmail.com>
2023-07-01 13:17:51 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 <<<<<<< HEAD
@@ -31,3 +32,6 @@ mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
+=======
+mkdir ~/bin && PATH=~/bin:$PATH && curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo && chmod a+x ~/bin/repo && repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault && git clone https://github.com/kitw4y/local_manifest.git --depth 1 -b evox .repo/local_manifests && repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 && source build/envsetup.sh && lunch evolution_lancelot-userdebug && mka evolution
+>>>>>>> parent of 5d04215 (Update build_rom.sh)
