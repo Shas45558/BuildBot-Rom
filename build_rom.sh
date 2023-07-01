d5909d8f6f956899b7a097ae2dda135ee43a0b1f
acex69 <abhinav.115260@gmail.com>
2023-07-01 13:17:59 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -2,6 +2,7 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
 <<<<<<< HEAD
+<<<<<<< HEAD
 git clone https://github.com/kitw4y/local_manifest.git --depth 1 -b evox .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
@@ -11,6 +12,8 @@ lunch evolution_lancelot-userdebug
 export TZ=Asia/Jakarta #put before last build command
 mka evolution
 =======
+=======
+>>>>>>> parent of 024c8c4 (Update build_rom.sh)
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 git clone https://github.com/galang8664/device-r --depth 1 -b evoxr device/xiaomi/lancelot
 git clone https://github.com/hentaiOS-MT6768/device_xiaomi_mt6768-common --depth 1 -b TwistedScarlett device/xiaomi/mt6768-common
@@ -28,6 +31,9 @@ source build/envsetup.sh
 lunch evolution_lancelot-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
+<<<<<<< HEAD
+>>>>>>> parent of 024c8c4 (Update build_rom.sh)
+=======
 >>>>>>> parent of 024c8c4 (Update build_rom.sh)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
